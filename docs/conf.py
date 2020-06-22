8423fc7fa2f963c6fd29a912c51cf2b410cc23aa
gandersen101 <30376815+gandersen101@users.noreply.github.com>
2020-06-22 17:29:30 -0500

    Initial CI deployment.

--- docs/conf.py
+++ docs/conf.py
@@ -2,3 +2,8 @@
 project = "hypermodern-python"
 author = "Grant Andersen (gandersen101)"
 copyright = f"2020, {author}"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
